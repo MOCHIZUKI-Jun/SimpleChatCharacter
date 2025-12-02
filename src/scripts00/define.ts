afcfca9330dbe8cef1cc200057a0b2f8d475ecdf
MOCHIZUKI Jun <j.m.bluefish@gmail.com>
2025-12-02 11:48:51 +0900

    update

--- src/scripts00/define.ts
+++ src/scripts00/define.ts
@@ -1,7 +1,7 @@
 import {GetColorCodeByRGB, GetColorCodeTextByRGB} from "../utility/colorUtility.ts";
 
 // バージョン番号
-export const VERSION_TEXT = "version 0.1.1";
+export const VERSION_TEXT = "version 0.1.2";
 
 // タイトル
 export const TITLE = "こんにちわ☆";
